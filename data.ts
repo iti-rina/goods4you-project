2ff3ed018c4c4e575b6c1104f0f66cb1c546f0c4
iti-rina <itigecheva.irina@gmail.com>
2024-09-16 03:39:28 +0700

    chore: add mock data fo faq

--- data.ts
+++ data.ts
@@ -17,3 +17,14 @@ const productItem: Product = {
 };
 
 export const products = new Array(12).fill(productItem);
+
+type FAQItem = {
+  title: string;
+  content: string
+}
+
+export const faqItems: FAQItem[] = [
+  {title: 'How can I track the status of my order?', content: 'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'},
+  {title: 'What payment methods do you accept?', content : 'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'},
+  {title: 'How can I return or exchange an item?', content: 'After placing your order, you will receive a confirmation email containing your order number and a tracking link. You can also log in to your account on our website and go to the "My Orders" section to track your delivery status.'},
+];
