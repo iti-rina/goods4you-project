7f35e963d410987b28b82f3792266f7f18857b9e
iti-rina <itigecheva.irina@gmail.com>
2024-09-16 01:13:55 +0700

    feat(style): update styles for hero section

--- src/pages/catalog/ui/Hero/ui.tsx
+++ src/pages/catalog/ui/Hero/ui.tsx
@@ -4,9 +4,11 @@ import styles from './hero.module.css';
 const Hero: React.FC = () => {
   return (
     <section className={styles.heroSection} >
-      <h1 className={styles.title}>Any products from famous brands with worldwide delivery</h1>
-      <p className={styles.subtitle}>We sell smartphones, laptops, clothes, shoes and many other products at low prices</p>
-      <Btn text='Go to shopping' isActive={true} />
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Any products from famous brands with worldwide delivery</h1>
+        <p className={styles.subtitle}>We sell smartphones, laptops, clothes, shoes and many other products at low prices</p>
+        <Btn text='Go to shopping' isActive={true} />
+      </div>
     </section>
   );
 }
