30a10065feecdecd4329bf80950f7e9326d22231
iti-rina <itigecheva.irina@gmail.com>
2024-09-15 21:54:25 +0700

    chore: add mock object for product

---
+++ data.ts
@@ -0,0 +1,17 @@
+import type { Product } from './src/share/types';
+
+export const productItem: Product = {
+  id: '1',
+  name: 'Essence Mascara Lash Princess',
+  description: 'The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.',
+  firstPrice: '9.99',
+  discountPrice: '7.17',
+  rating: 4,
+  categories: ['electronics', 'selfie accessories'],
+  stock: true,
+  stockQnt: 5,
+  warranty: '1 month warranty',
+  ship: 'Ships in 1 month',
+  images: [],
+  coverImage: ''
+}
