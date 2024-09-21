10b2388f6e458e8b4b9147633148d157392816dc
iti-rina <itigecheva.irina@gmail.com>
2024-09-22 00:44:18 +0700

    feat: add handler prop

--- src/share/ui/btn/ui.tsx
+++ src/share/ui/btn/ui.tsx
@@ -5,17 +5,18 @@ type BtnValues = {
   iconName: string
   isActive: boolean
   styleProp: string
+  onClick: () => void
 }
 
 type IconValues = {
   name?: string
 }
 
-const Btn: React.FC<Partial<BtnValues>> = ({ text, iconName, isActive=true, styleProp='' }) => {
+const Btn: React.FC<Partial<BtnValues>> = ({ text, iconName, isActive=true, styleProp='', onClick }) => {
   const Icon: React.FC<IconValues> = ({name = 'default'}) => <div>{name}</div>;
 
   return (
-    <button className={`${styles.btn} ${ !isActive ? styles.btnInactive : '' } ${ !text ? styles.btnSquare : ''} ${styleProp}`}>
+    <button className={`${styles.btn} ${ !isActive ? styles.btnInactive : '' } ${ !text ? styles.btnSquare : ''} ${styleProp}`} onClick={onClick}>
       { text 
       ? <>{text}</>
       : <Icon name={iconName} />
