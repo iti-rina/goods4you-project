efca943f7c86576a66d5478ff2e1ed0c0cc106a5
iti-rina <itigecheva.irina@gmail.com>
2024-09-21 17:32:33 +0700

    feat: add provider to work with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { AppRoutes } from './app/routes'
+import { Provider } from 'react-redux';
+import { AppRoutes } from './app/routes';
+import { store } from './app/store';
 
 function App() {
 
   return (
-    <AppRoutes />
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
   )
 }
 
