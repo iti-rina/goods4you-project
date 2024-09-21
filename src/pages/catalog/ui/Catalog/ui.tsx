595072dae4bbeef6b1ab257e8b6388746061221a
iti-rina <itigecheva.irina@gmail.com>
2024-09-22 00:44:55 +0700

    feat: add pagination

--- src/pages/catalog/ui/Catalog/ui.tsx
+++ src/pages/catalog/ui/Catalog/ui.tsx
@@ -1,23 +1,41 @@
 import ProductList from '../ProductList/ui';
-import { products } from '../../../../../data';
 import styles from './Catalog.module.css';
 import { SearchBar } from '../../../../features/search-bar';
 import { Btn } from '../../../../share';
 import { useSearchByTitleQuery } from '../../model/catalogSlice';
 import { useState, useEffect, ChangeEvent } from 'react';
 import { debounce } from 'lodash';
+import { ITEMS_PER_PAGE } from '../../api';
 
 const Catalog: React.FC = () => {
   const [ searchQuery, setSearchQuery ] = useState('');
+  const [ debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
+  const debouncedSearch = debounce(query => setDebouncedSearchQuery(query), 300);
+  
+  const [ skip, setSkip ] = useState(0);
+  const [ products, setProducts ] = useState([]);
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
   }
 
-  const [ debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
-  const debouncedSearch = debounce(query => setDebouncedSearchQuery(query), 300);
+  const handleShowMoreClick = () => {
+    setSkip(prevVal => prevVal + ITEMS_PER_PAGE);
+  }
+  
+  const { data, error, isLoading } = useSearchByTitleQuery({ title: debouncedSearchQuery, skip }, { refetchOnMountOrArgChange: true });
 
-  const { data, error, isLoading } = useSearchByTitleQuery(debouncedSearchQuery);
-  console.log(data);
+  useEffect(() => {
+    if (data) {
+      setProducts(prevProducts => {
+        if (skip === 0) {
+          return data.products;
+        } else {
+          return [...prevProducts, ...data.products]
+        }
+      });
+    }
+  }, [data, skip]);
 
   useEffect(() => {
     debouncedSearch(searchQuery);
@@ -25,6 +43,10 @@ const Catalog: React.FC = () => {
       debouncedSearch.cancel();
     };
   }, [searchQuery, debouncedSearch]);
+
+  useEffect(() => {
+    setSkip(0);
+  }, [debouncedSearchQuery]);
   
   return (
     <section id='catalog' className={styles.wrapper}>
@@ -33,8 +55,15 @@ const Catalog: React.FC = () => {
         query={searchQuery}
         onChange={handleChange}
       />
-      <ProductList products={data.products} />
-      <Btn text='Show more' styleProp={styles.btn} />
+
+      {isLoading && <p>Загрузка...</p>}
+      {error && <p>Ошибка: {error}</p>}
+
+      <ProductList products={products} />
+
+      {(!isLoading && data && data?.products.length === ITEMS_PER_PAGE) ?
+        <Btn text='Show more' styleProp={styles.btn} onClick={handleShowMoreClick}/> : <></>
+      }
     </section>
   );
 }
