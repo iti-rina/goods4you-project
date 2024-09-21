f2212ad7bdcf590f3e52a4ce9f87177dc79c1681
iti-rina <itigecheva.irina@gmail.com>
2024-09-21 23:40:39 +0700

    feat: change query parameters to handle pagination

--- src/pages/catalog/model/catalogSlice.ts
+++ src/pages/catalog/model/catalogSlice.ts
@@ -1,12 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../api';
-
+import { BASE_URL, LIMIT } from '../api';
 export const catalogApi = createApi({
   reducerPath: 'catalogApi',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     searchByTitle: builder.query({
-      query: (title) => `?q=${title}`,
+      query: ({ title, skip }) => `?q=${title}&${LIMIT ? `limit=${LIMIT}` : ''}&skip=${skip}`,
     }),
   }),
 });
