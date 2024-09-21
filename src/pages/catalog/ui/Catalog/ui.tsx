129be20970be2e86951207cd923a80926177a9db
iti-rina <itigecheva.irina@gmail.com>
2024-09-21 19:19:12 +0700

    feat: add fetching product using RTL Query and debounce function from lodash

--- src/pages/catalog/ui/Catalog/ui.tsx
+++ src/pages/catalog/ui/Catalog/ui.tsx
@@ -3,14 +3,37 @@ import { products } from '../../../../../data';
 import styles from './Catalog.module.css';
 import { SearchBar } from '../../../../features/search-bar';
 import { Btn } from '../../../../share';
+import { useSearchByTitleQuery } from '../../model/catalogSlice';
+import { useState, useEffect, ChangeEvent } from 'react';
+import { debounce } from 'lodash';
 
 const Catalog: React.FC = () => {
+  const [ searchQuery, setSearchQuery ] = useState('');
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  }
+
+  const [ debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
+  const debouncedSearch = debounce(query => setDebouncedSearchQuery(query), 300);
+
+  const { data, error, isLoading } = useSearchByTitleQuery(debouncedSearchQuery);
+  console.log(data);
+
+  useEffect(() => {
+    debouncedSearch(searchQuery);
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [searchQuery, debouncedSearch]);
   
   return (
     <section id='catalog' className={styles.wrapper}>
       <h2 className={styles.title}>Catalog</h2>
-      <SearchBar />
-      <ProductList products={products} />
+      <SearchBar 
+        query={searchQuery}
+        onChange={handleChange}
+      />
+      <ProductList products={data.products} />
       <Btn text='Show more' styleProp={styles.btn} />
     </section>
   );
