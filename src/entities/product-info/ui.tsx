d942ca220c5bd7111dc1f89c340b2d3db0f95cf1
iti-rina <itigecheva.irina@gmail.com>
2024-09-22 20:22:23 +0700

    feat: make function available for export to use in othe modules

--- src/entities/product-info/ui.tsx
+++ src/entities/product-info/ui.tsx
@@ -7,7 +7,7 @@ type ProductInfoValues = {
   product: Product;
 }
 
-function roundUpToTwoDecimalPlaces(num: number) {
+export function roundUpToTwoDecimalPlaces(num: number) {
   return Math.ceil(num * 100) / 100;
 }
 
