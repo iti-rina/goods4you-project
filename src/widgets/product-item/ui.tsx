900a38acfcff486ba0a89918ef0bc545288d0d22
iti-rina <itigecheva.irina@gmail.com>
2024-09-23 01:12:32 +0700

    feat: add check if product is in cart

--- src/widgets/product-item/ui.tsx
+++ src/widgets/product-item/ui.tsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
 import { Btn, Product } from '../../share';
 import styles from './productItem.module.css';
+import { useSelector } from 'react-redux';
+import { selectCartProducts } from '../../pages/cart/model/cartSlice';
+import { Control } from '../cart-item';
 
 type ProductProps = {
   product: Product
 }
 const ProductCard: React.FC<ProductProps> = ({ product }) => {
+  const cartProducts = useSelector(selectCartProducts);
+
+  const findInCart = cartProducts.find(el => el.id === product.id);
+  
   return (
     <Link to={`/product/${product.id}`}>
       <article className={styles.card}>
@@ -13,11 +20,13 @@ const ProductCard: React.FC<ProductProps> = ({ product }) => {
           <img src={product.thumbnail} alt={`Фотография-обложка товара ${product.title}`} className={styles.img} />
         </div>
         <div className={styles.infoWrapper}>
-          <div className={styles.info}>
+          <div className={`${styles.info} ${findInCart ? styles.cuttedInfo : ''}`}>
             <p className={styles.name}>{product.title}</p>
             <p className={styles.price}>${product.discountPercentage}</p>
           </div>
-          <Btn iconName='cart' />
+          { 
+            findInCart ? <Control count={findInCart.quantity} /> : <Btn iconName='cart' />
+          }
         </div>
       </article>
     </Link>
