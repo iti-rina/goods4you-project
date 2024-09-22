e7b5699297485fbe04ee000ab3708286a0522b85
iti-rina <itigecheva.irina@gmail.com>
2024-09-22 20:16:44 +0700

    feat: add minus and plus icons available in button component

--- src/share/ui/btn/ui.tsx
+++ src/share/ui/btn/ui.tsx
@@ -1,8 +1,10 @@
 import styles from './btn.module.css';
+import { MinusIcon } from '../../../assets/icons/minus';
+import { PlusIcon } from '../../../assets/icons/plus';
 
 type BtnValues = {
   text: string
-  iconName: string
+  iconName: string 
   isActive: boolean
   styleProp: string
   onClick: () => void
@@ -13,7 +15,13 @@ type IconValues = {
 }
 
 const Btn: React.FC<Partial<BtnValues>> = ({ text, iconName, isActive=true, styleProp='', onClick }) => {
-  const Icon: React.FC<IconValues> = ({name = 'default'}) => <div>{name}</div>;
+  const Icon: React.FC<IconValues> = ({name = 'default'}) => {
+    if (name === 'minus') {
+      return MinusIcon;
+    } else if (name === 'plus') {
+      return PlusIcon;
+    }
+  };
 
   return (
     <button className={`${styles.btn} ${ !isActive ? styles.btnInactive : '' } ${ !text ? styles.btnSquare : ''} ${styleProp}`} onClick={onClick}>
