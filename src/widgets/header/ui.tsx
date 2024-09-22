34d40c3c346f8cd6921f5b7d6cd55994053b8c99
iti-rina <itigecheva.irina@gmail.com>
2024-09-22 21:00:46 +0700

    feat: add fecthCart method to use in header

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,12 +1,23 @@
 import { LinkComponent } from "../../share/ui/link";
 import styles from './header.module.css';
 import { CartIcon } from "../../assets/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchCart } from "../../pages/cart/model/cartSlice";
 
 type HeaderPropsType = {
   user: string
 }
 
 const Header: React.FC<HeaderPropsType> = ({ user }) => {
+  const dispatch = useDispatch();
+  const { cart, loading, error } = useSelector((state) => state.cart);
+  console.log(cart);
+
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
+
   return(
     <header className={styles.header}>
       <LinkComponent text='Goods4you' url='#' isLogo={true} />
@@ -16,7 +27,10 @@ const Header: React.FC<HeaderPropsType> = ({ user }) => {
           <li><LinkComponent text='FAQ' url='#'/></li>
           <li className={styles.cartWrapper}>
             <LinkComponent text='Cart' url='/cart'/>
-            <div className={styles.cartIcon}>{CartIcon}</div>
+            <div className={styles.cartIcon}>
+              {CartIcon}
+              {cart ? <span className={styles.cartQnt}>{cart.totalQuantity > 100 ? '99+' : cart.totalQuantity}</span> : null}
+            </div>
           </li>
           <li><LinkComponent text={user} url='#'/></li>
         </ul>
