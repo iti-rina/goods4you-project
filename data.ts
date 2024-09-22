64a6ed12a2ff59759154469e1af6235f1c84f4c7
iti-rina <itigecheva.irina@gmail.com>
2024-09-22 23:23:05 +0700

    chore: change mock-data to use in storybook

--- data.ts
+++ data.ts
@@ -1,20 +1,25 @@
-import type { Product } from './src/share/types';
-
-export const productItem: Product = {
-  id: '1',
-  name: 'Essence Mascara Lash Princess',
-  description: 'The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.',
-  firstPrice: '9.99',
-  discountPrice: '7.17',
-  rating: 4,
-  categories: ['electronics', 'selfie accessories'],
-  stock: true,
-  stockQnt: 5,
-  warranty: '1 month warranty',
-  ship: 'Ships in 1 month',
-  images: ['./src/assets/images/catalog.png', './src/assets/images/catalog.png', './src/assets/images/catalog.png', './src/assets/images/catalog.png', './src/assets/images/catalog.png', './src/assets/images/catalog.png'],
-  coverImage: './src/assets/images/catalog.png'
-};
+export const productItem = {
+  "id": '1',
+  "title": "Essence Mascara Lash Princess",
+  "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+  "category": "beauty",
+  "price": 9.99,
+  "discountPercentage": 7.17,
+  "rating": 4.94,
+  "stock": 5,
+  "tags": [
+      "beauty",
+      "mascara"
+  ],
+  "brand": "Essence",
+  "warrantyInformation": "1 month warranty",
+  "shippingInformation": "Ships in 1 month",
+  "availabilityStatus": "Low Stock",
+  "images": [
+      "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png"
+}
 
 export const products = new Array(12).fill(productItem);
 
