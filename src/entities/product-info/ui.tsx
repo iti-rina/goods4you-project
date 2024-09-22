465ab1fdb4ada9c483c3399fb256bec88e4d91ee
iti-rina <itigecheva.irina@gmail.com>
2024-09-22 17:29:51 +0700

    feat: update product variables to use api; create functions to work with rating and prices

--- src/entities/product-info/ui.tsx
+++ src/entities/product-info/ui.tsx
@@ -1,39 +1,61 @@
 import styles from './productInfo.module.css';
 import { Product } from '../../share'
 import {AddToCart} from '../../features'
+import { StarRating } from '../../share/ui/rating';
 
 type ProductInfoValues = {
   product: Product;
 }
 
+function roundUpToTwoDecimalPlaces(num: number) {
+  return Math.ceil(num * 100) / 100;
+}
+
+function roundToClosestNumber(num: number) {
+  const fractionalPart = num % 1;
+  let result = num;
+  if (fractionalPart < 0.5) {
+    result = Math.floor(num);
+  } else {
+    result = Math.ceil(num);
+  }
+  return result;
+}
 
 const ProductInfo: React.FC<ProductInfoValues> = ({ product }) => {
+  const discountPrice = product.price * (1 - product.discountPercentage / 100);
+  const roundedPrice = roundUpToTwoDecimalPlaces(discountPrice);
+
+  const rating = roundToClosestNumber(product.rating);
+
+  
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{product.name}</h1>
+      <h1 className={styles.title}>{product.title}</h1>
       <div className={styles.secondInfo}>
-        <div className={styles.rating}><img src={'./src/assets/icons/rating.svg'} alt={'rating'}/></div>
+        <div className={styles.rating}><StarRating rating={rating}/></div>
         <div className={styles.categories}>
-          {product.categories.map((item, i) =>
+          {product.tags.map((item, i) =>
             <p className={styles.item} key={i}>
-              {product.categories.length -1 === i ? item : `${item},`}
+              {product.tags.length -1 === i ? item : `${item},`}
             </p>
           )}
         </div>
       </div>
-      <div className={styles.stock}>In Stock - Only 5 left!</div>
+      <div className={styles.stock}>In Stock - Only {product.stock} left!</div>
       <div className={styles.description}>{product.description}</div>
       <div className={styles.thirdInfo}>
-        <div className={styles.warranty}>{product.warranty}</div>
-        <div className={styles.ship}>{product.ship}</div>
+        <div className={styles.warranty}>{product.warrantyInformation}</div>
+        <div className={styles.ship}>{product.shippingInformation}</div>
       </div>
       <div className={styles.addCart}>
         <div className={styles.price}>
           <div>
-            <div className={styles.discountPrice}>${product.discountPrice}</div>
-            <div className={styles.firstPrice}>${product.firstPrice}</div>
+            <div className={styles.discountPrice}>${roundedPrice}</div>
+            <div className={styles.firstPrice}>${product.price}</div>
           </div>
-          <div className={styles.discount}>Your discount: <span className={styles.bold}>14.5%</span></div>
+          <div className={styles.discount}>Your discount: <span className={styles.bold}>{product.discountPercentage}%</span></div>
         </div>
         <div className={styles.add}>
           <AddToCart type={'text'} id={product.id} />
