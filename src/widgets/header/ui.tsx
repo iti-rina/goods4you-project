700fd589c69ee81fee4c0516c840c3cfb26dc091
iti-rina <itigecheva.irina@gmail.com>
2024-09-29 01:58:05 +0700

    fix: make nav bar available only for authorized person

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -4,12 +4,15 @@ import { CartIcon } from "../../assets/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchCart } from "../../pages/cart/model/cartSlice";
+import { useLocation } from "react-router-dom";
 
 type HeaderPropsType = {
   user: string
 }
 
 const Header: React.FC<HeaderPropsType> = ({ user }) => {
+  const { pathname } = useLocation();
+
   const dispatch = useDispatch();
   const { cart } = useSelector((state) => state.cart);
 
@@ -20,20 +23,24 @@ const Header: React.FC<HeaderPropsType> = ({ user }) => {
   return(
     <header className={styles.header}>
       <LinkComponent text='Goods4you' url='#' isLogo={true} />
-      <nav aria-label='Основная навигация'>
-        <ul className={styles.menuList}>
-          <li><LinkComponent text='Catalog' url='/'/></li>
-          <li><LinkComponent text='FAQ' url='#'/></li>
-          <li className={styles.cartWrapper}>
-            <LinkComponent text='Cart' url='/cart'/>
-            <div className={styles.cartIcon}>
-              {CartIcon}
-              {cart ? <span className={styles.cartQnt}>{cart.totalQuantity > 100 ? '99+' : cart.totalQuantity}</span> : null}
-            </div>
-          </li>
-          <li><LinkComponent text={user} url='#'/></li>
-        </ul>
-      </nav>
+      { pathname !== '/login'
+        ? (<nav aria-label='Основная навигация'>
+            <ul className={styles.menuList}>
+              <li><LinkComponent text='Catalog' url='/'/></li>
+              <li><LinkComponent text='FAQ' url='#'/></li>
+              <li className={styles.cartWrapper}>
+                <LinkComponent text='Cart' url='/cart'/>
+                <div className={styles.cartIcon}>
+                  {CartIcon}
+                  {cart ? <span className={styles.cartQnt}>{cart.totalQuantity > 100 ? '99+' : cart.totalQuantity}</span> : null}
+                </div>
+              </li>
+              <li><LinkComponent text={user} url='#'/></li>
+            </ul>
+          </nav>)
+        : <></>
+      }
+
     </header>
   );
 }
