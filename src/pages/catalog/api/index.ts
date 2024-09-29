2550d08be6097011abf76b6a6b908556bc489e83
iti-rina <itigecheva.irina@gmail.com>
2024-09-29 15:08:01 +0700

    feat: make catalog fetch request under auth

--- src/pages/catalog/api/index.ts
+++ src/pages/catalog/api/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://dummyjson.com/products/search';
+export const BASE_URL = 'https://dummyjson.com/auth/products/search';
 export const LIMIT = 12;
 export const ITEMS_PER_PAGE = 12;
--- src/pages/catalog/model/catalogSlice.ts
+++ src/pages/catalog/model/catalogSlice.ts
@@ -2,7 +2,13 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL, LIMIT } from '../api';
 export const catalogApi = createApi({
   reducerPath: 'catalogApi',
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: BASE_URL,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${window.localStorage.getItem('accessToken')}`
+    },
+  }),
   endpoints: (builder) => ({
     searchByTitle: builder.query({
       query: ({ title, skip }) => `?q=${title}&${LIMIT ? `limit=${LIMIT}` : ''}&skip=${skip}`,
