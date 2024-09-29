ec3e8a25f35486ae260f2101904e9fe71078c57c
iti-rina <itigecheva.irina@gmail.com>
2024-09-30 00:18:40 +0700

    feat: create asyncThunk for manage productsQuantity in cart

--- src/pages/cart/model/cartSlice.ts
+++ src/pages/cart/model/cartSlice.ts
@@ -1,19 +1,37 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { USER_ID } from '../api';
 
-export const fetchCart = createAsyncThunk('cart/fetchCart', async () => {
-  const response = await fetch(`https://dummyjson.com/auth/carts/user/${USER_ID}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${window.localStorage.getItem('accessToken')}`
-    }
+const requestHeaders = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${window.localStorage.getItem('accessToken')}`
+}
+
+export const fetchCart = createAsyncThunk('cart/fetchCart', async (id: number) => {
+  const response = await fetch(`https://dummyjson.com/auth/carts/user/${id}`, {
+    headers: requestHeaders
   });
   if (!response.ok) {
-    throw new Error('Ошибка при загрузке данных');
+    throw new Error('Error while fetching cart products');
   }
   const data = await response.json();
   return data.carts[0];
 });
+
+export const manageCart = createAsyncThunk('cart/manageCart', async ({id, products}) => {
+  const response = await fetch(`https://dummyjson.com/auth/carts/${id}`, {
+    headers: requestHeaders,
+    method: 'PUT',
+    body: JSON.stringify({
+      'merge': false,
+      'products': products
+    })
+  });
+  if (!response.ok) {
+    throw new Error('Error while fetching cart products');
+  }
+  const data = await response.json();
+  return data;
+});
+
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -36,9 +54,22 @@ const cartSlice = createSlice({
       .addCase(fetchCart.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
-      });
+      })
+      .addCase(manageCart.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(manageCart.fulfilled, (state, action) => {
+        state.loading = false;
+        state.cart = action.payload;
+      })
+      .addCase(manageCart.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
   },
 });
 
 export const selectCartProducts = (state) => state.cart.cart.products;
+export const selectCartId = (state) => state.cart.cart.id;
 export default cartSlice.reducer;
